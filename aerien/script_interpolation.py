e207c93e55b12402d23cbef42833ee6f521494b9
ClementAlba <93247842+ClementAlba@users.noreply.github.com>
2021-12-20 16:46:31 +0100

    Ajout fichier config dev
    Ajout du fichier de config de la phase de développement

--- aerien/script_interpolation.py
+++ aerien/script_interpolation.py
@@ -1,61 +1,65 @@
 from grass.pygrass.modules import Module
 
-from grass.script import core as grass
+from grass.script import core as grass
+
+import json
 
+with open("D:\\Documents\\GitHub\\scan_process\\aerien\\config_dev.json") as jsonFile:
+    config = json.load(jsonFile)
+    jsonFile.close()
 
-def create_new_location():
-    grass.create_location("D:\\APPS\\WinGRASS", "location_heron", 2154)
+def create_new_location():
+    config_location = config.get("create_new_location")
+
+    grass.create_location(config_location.get("gisdbase"), config_location.get("location_name"), config_location.get("epsg"))
 
     Module("g.mapset",
            flags="c",
-           mapset="MAPSET_HERON",
-           location="location_heron"
+           mapset=config_location.get("g.mapset").get("mapset_name"),
+           location=config_location.get("location_name")
           )
 
+def import_file():
+    config_import = config.get("import_file").get("v.in.lidar")
 
-def import_file():
     Module("v.in.lidar",
            flags="tbo",
-           input="D:\\Documents\\MISSIONS\\interpolation_raster\\input\\heron_min2.las",
-           output="heron_import@MAPSET_HERON",
-
-           class_filter=2,
-
+           input=config_import.get("input"),
+           output=config_import.get("output"),
+           class_filter=config_import.get("class_filter"),
            overwrite=True
           )
 
+def interpolation():
+    config_region = config.get("interpolation").get("g.region")
+    config_surf = config.get("interpolation").get("v.surf.rst")
+    config_gdal = config.get("interpolation").get("r.out.gdal")
 
-
-def command():
     Module("g.region",
-           vector="heron_import@MAPSET_HERON",
-           n=7060385,
-           s=7059161,
-           e=713007,
-           w=711393,
-           res=0.3
+           vector=config_region.get("vector"),
+           res=config_region.get("res")
           )
 
 
     Module("v.surf.rst",
-           input="heron_import@MAPSET_HERON",
-           elevation="heron_elevation",
-           smooth=2,
-           dmin=10,
-           segmax=1200,
-           npmin=1600
+           input=config_surf.get("input"),
+           elevation=config_surf.get("elevation"),
+           smooth=config_surf.get("smooth"),
+           dmin=config_surf.get("dmin"),
+           segmax=config_surf.get("segmax"),
+           npmin=config_surf.get("npmin"),
+           overwrite=True
           )
 
 
     Module("r.out.gdal",
-           input="heron_elevation@MAPSET_HERON",
-           output="D:\\Documents\\MISSIONS\\interpolation_raster\\output\\heron.tif",
-           format="GTiff"
+           input=config_gdal.get("input"),
+           output=config_gdal.get("output"),
+           format=config_gdal.get("format"),
+           overwrite=True
           )
-
-
 
 if __name__ == "__main__":
     create_new_location()
     import_file()
-    command()
+    interpolation()
